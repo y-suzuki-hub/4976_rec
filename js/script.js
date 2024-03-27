228b7b866a6c576b30cd7208b1789bd465d4e8e4
y-suzuki <suzuki@4976.co.jp>
2024-03-27 09:22:05 +0900

    [Update]

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//ファーストビューメイン画像
+setTimeout(function(){
+	$(".img-mv").stop().animate({opacity:'1'},1000);
+},100);
+
+//header_wrap
+$(window).scroll(function () {
+	if($(window).scrollTop() > 400) {
+		$('#header_wrap').animate({'top':'0px'},{ duration: 'fast', easing: 'swing', });
+	}
+});
+
 $(".openbtn6").click(function () {
     $(this).toggleClass('active');
 });
