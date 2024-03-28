8e5370b6c89515a39baa4bd122fbfa04b4dc98ed
y-suzuki <suzuki@4976.co.jp>
2024-03-28 10:52:40 +0900

    [Update]4976

--- js/script.js
+++ js/script.js
@@ -113,11 +113,57 @@ $('.theory__index--close').on('click', function() {
   });
   */
 
+  $(function() {
+    var offset = $('#nav-4976').offset();
+    var scrollThreshold = offset.top - 65; // 65pxスクロールしたら固定にする
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > scrollThreshold) {
+            $('#nav-4976').addClass('fixed');
+        } else {
+            $('#nav-4976').removeClass('fixed');
+        }
+    });
+});
+
+
+//ページ上部へ　スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 300){//上から200pxスクロールしたら
+      $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+      $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+    }else{
+      if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+        $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+        $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+      }
+    }
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+  });
+  
+  // ページが読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function () {
+    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+  });
+  
+  // #page-topをクリックした際の設定
+  $('#page-top a').click(function () {
+      $('body,html').animate({
+          scrollTop: 0//ページトップまでスクロール
+      }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+      return false;//リンク自体の無効化
+  });
+
 
 // #で始まるアンカーをクリックした場合に処理 
 $('a[href^="#"]').click(function(){
-    // 移動先を90px上にずらす
-    var adjust = -90;
+    // 移動先を110px上にずらす
+    var adjust = -110;
     // スクロールの速度
     var speed = 400; // ミリ秒
     // アンカーの値取得
