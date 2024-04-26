de4162af4090ef653ae23f28eb0ac21bbf86bfdc
y-suzuki <suzuki@4976.co.jp>
2024-04-26 14:55:55 +0900

    add

---
+++ pre01/js/fv-01.js
@@ -0,0 +1,5 @@
+$("#particle").particleText({
+    text: "Welcome<br>to 4976", // 表示させたいテキスト。改行の場合は<br>追加
+    colors:["#fff","#fff", "#fff"], // パーティクルの色を複数指定可能
+    speed: "slow", // slow, middle, high の3つから粒子が集まる速さを選択
+    });
