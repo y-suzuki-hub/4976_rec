90bc6e7bea43d1627b361984fbaca5628d416341
y-suzuki <suzuki@4976.co.jp>
2024-04-26 16:31:26 +0900

    update

---
+++ pre01/js/test-slick.js
@@ -0,0 +1,40 @@
+$(function () {
+    $(".js-slick01").slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase: 'linear',
+        slidesToShow: 3,
+        swipe: false,
+        pauseOnFocus: false, 
+        pauseOnHover: false,
+        arrows: false,
+    });
+});
+$(function () {
+    $(".js-slick02").slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase: 'linear',
+        slidesToShow: 3,
+        swipe: false,
+        pauseOnFocus: false, 
+        pauseOnHover: false,
+        arrows: false,
+        rtl: true,
+    });
+});
+$(function () {
+    $(".js-slick03").slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase: 'linear',
+        slidesToShow: 3,
+        swipe: false,
+        pauseOnFocus: false, 
+        pauseOnHover: false,
+        arrows: false,
+    });
+});
