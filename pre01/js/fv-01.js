7d5a3e24c8616237973515b4c3438ed7ff010b66
y-suzuki <suzuki@4976.co.jp>
2024-05-01 11:41:10 +0900

    #

--- pre01/js/fv-01.js
+++ pre01/js/fv-01.js
@@ -1,5 +1,7 @@
-$("#particle").particleText({
-    text: "Welcome<br>to 4976", // 表示させたいテキスト。改行の場合は<br>追加
-    colors:["#fff","#fff"], // パーティクルの色を複数指定可能
-    speed: "slow", // slow, middle, high の3つから粒子が集まる速さを選択
-    });
+setTimeout(function () {
+    $("#particle").particleText({
+        text: "Welcome<br>to 4976", // 表示させたいテキスト。改行の場合は<br>追加
+        colors:["#fff","#fff"], // パーティクルの色を複数指定可能
+        speed: "slow", // slow, middle, high の3つから粒子が集まる速さを選択
+        });
+}, 500);
