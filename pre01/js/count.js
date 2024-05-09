106171a9202e5dda269ebf52fdb961edb3b84091
y-suzuki <suzuki@4976.co.jp>
2024-05-09 17:20:26 +0900

    a

--- pre01/js/count.js
+++ pre01/js/count.js
@@ -81,3 +81,24 @@ $('#count-up04').on('inview', function(event, isInView) {
 		$('#count-up04 .canma').removeClass('is-animated');
 	}
 });
+
+$('#count-up05').on('inview', function(event, isInView) {
+	if (isInView) {
+		//要素が見えたときに実行する処理
+		$("#count-up05 .count-up-item").each(function(){
+			$(this).prop('Counter',0).animate({//0からカウントアップ
+		        Counter: $(this).text()
+		    }, {
+				// スピードやアニメーションの設定
+		        duration: 10000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
+		        easing: 'swing',//動きの種類。他にもlinearなど設定可能
+		        step: function (now) {
+		            $(this).text(Math.ceil(now));
+		        }
+		    });
+            $('#count-up05 .canma').addClass('is-animated');
+		});
+	} else {
+		$('#count-up05 .canma').removeClass('is-animated');
+	}
+});
